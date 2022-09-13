7e1bdf2b1c073f9f96d36e879764c5a59a55fafc
bngams <n.boris@devidia.net>
2022-09-13 12:16:30 +0200

    install material and start to refactor app in multiple components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,24 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
-  date = new Date();
-  imgUrl = 'https://picsum.photos/200';
-  imgDesc = 'Description';
-  buttonDisabled = false;
-  iconBgColor = '#000';
-
-  words = [
-    'hello',
-    'world',
-    '!'
-  ]
-
-
-  buttonClicked(): void {
-    alert('Button clicked!');
-  }
-
-  toggleButtonStatus(): void {
-    this.buttonDisabled = !this.buttonDisabled;
-  }
 }
