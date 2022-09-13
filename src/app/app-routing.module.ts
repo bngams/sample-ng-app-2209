a3ed3c95f8a0458970867402d4e18623791f4fac
bngams <n.boris@devidia.net>
2022-09-13 12:30:10 +0200

    start routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GetStartedComponent } from './components/pages/get-started/get-started.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'get-started', component: GetStartedComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
